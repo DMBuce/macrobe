73810ae8ac69ab89f11cce89eb4e6d3b4ffba72e
Buce <dmbuce@gmail.com>
2022-05-18 11:02:09 -0500

    Add dynamic macros

---
+++ ooo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+# define some vars
+mailto=$'department@example.com\tanother-dept@example.com'
+cc='boss@example.com'
+
+# schedule start date
+start="$(pickdate '%A %-m/%-d')"
+humanstart="$(date -d "$start" +'%a, %b %-e')"
+
+# schedule end date
+epochend="$(pickdate %s)"
+end="$(date -d @$epochend +'%A %-m/%-d')"
+humanend="$(date -d "$end" +'%a, %b %-e')"
+humanback="$(date -d @$(($epochend + 24*3600)) +'%A')"
+
+# print template
+cat <<EOF
+$mailto		${cc}	OOO $start thru $end	I will be out of the office from $humanstart to $humanend, returning $humanback.
+
+Have a good one.
+EOF
+
